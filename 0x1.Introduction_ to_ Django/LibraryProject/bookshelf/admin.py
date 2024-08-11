086273969257f0138f720ef0dd8abd39a7a5dc7f
mr-r <rbbagyei@gmail.com>
2024-08-11 21:09:13 +0000

    Utilizing the Django Admin Interface: list_filter has been added to the custom admin configurations for filtering data.

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -5,5 +5,6 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
   list_display = ('title', 'author', 'publication_year')
   search_fields = ('title', 'author')
+  list_filter = ('author', 'publication_year')
 
 admin.site.register(Book, BookAdmin)
