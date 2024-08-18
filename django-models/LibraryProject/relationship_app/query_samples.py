c13ff03322c21e613421529434279e0cd354b229
mr-r <rbbagyei@gmail.com>
2024-08-18 20:42:26 +0000

    0.Implementing Advanced Model Relationships in Django: Query_samples.py now have been refactored into functions to accomodate using variable names required by checker.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,22 +1,24 @@
 from . import models
 
 # Querying all books by a specific Author
+def books_by_author(author_name):
+  author = models.Author.objects.get(name=author_name)
+  books_by_author = author.books.all()
 
-author = models.Author.objects.get(name = '')
-books_by_author = author.books.all()
-
-for book in books_by_author:
-  print(book.name)
+  for book in books_by_author:
+    print(book.name)
 
 
 # Listing all books in a Library
-library = models.Library.objects.get(name = '')
-books_in_a_library = library.books.all()
+def books_in_a_library(library_name):
+  library = models.Library.objects.get(name=library_name)
+  books_in_a_library = library.books.all()
 
-for book in books_in_a_library:
-  print(book.name)
+  for book in books_in_a_library:
+    print(book.name)
 
 # Retrieving the Librarian for a library
-library = models.Library.objects.get(name = '')
-librarian_for_the_library = library.librarian
-print(librarian_for_the_library.name)
+def librarian_for_a_library(library_name):
+  library = models.Library.objects.get(name=library_name)
+  librarian_for_the_library = library.librarian
+  print(librarian_for_the_library.name)
