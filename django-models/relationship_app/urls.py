f7a13bd85feca3d5ff61e711322aa4b22699fa50
mr-r <rbbagyei@gmail.com>
2024-08-18 22:41:08 +0000

    2. Implementing User Authentication in Django.

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
+from .views import user_login
+from .views import user_logout
+from .views import user_register
 
 urlpatterns = [
   path('books/', list_books, name='list_books'),
-  path('library/<int:pk>', LibraryDetailView.as_view(), name='library_list')
+  path('library/<int:pk>', LibraryDetailView.as_view(), name='library_list'),
+  path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', user_register, name='register')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 # Create your views here.
 
 def list_books(request):
@@ -13,3 +21,29 @@ class LibraryDetailView(DetailView):
   model = Library
   template_name = 'relationship_app/library_detail.html'
   context_object_name = 'library'
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home') 
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('logout')
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
