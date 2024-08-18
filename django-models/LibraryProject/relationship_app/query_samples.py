d9c94694f2d753f82ccc64f9b5c586daf3900b90
mr-r <rbbagyei@gmail.com>
2024-08-18 20:45:30 +0000

    0. Implementing Advanced Model Relationships in Django: Imports are now done per checker requirements

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,8 @@
-from . import models
+from .models import Author, Book, Library, Librarian
 
 # Querying all books by a specific Author
 def books_by_author(author_name):
-  author = models.Author.objects.get(name=author_name)
+  author = Author.objects.get(name=author_name)
   books_by_author = author.books.all()
 
   for book in books_by_author:
@@ -11,7 +11,7 @@ def books_by_author(author_name):
 
 # Listing all books in a Library
 def books_in_a_library(library_name):
-  library = models.Library.objects.get(name=library_name)
+  library = Library.objects.get(name=library_name)
   books_in_a_library = library.books.all()
 
   for book in books_in_a_library:
@@ -19,6 +19,6 @@ def books_in_a_library(library_name):
 
 # Retrieving the Librarian for a library
 def librarian_for_a_library(library_name):
-  library = models.Library.objects.get(name=library_name)
+  library = Library.objects.get(name=library_name)
   librarian_for_the_library = library.librarian
   print(librarian_for_the_library.name)
