ca861dd7a3962fc1001784e001fc241b713af8f9
mr-r <rbbagyei@gmail.com>
2024-08-18 22:49:03 +0000

    2. Implementing User Authentication in Django: Imports separated

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
-from .views import user_login
-from .views import user_logout
-from .views import user_register
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from . import views
 
 urlpatterns = [
   path('books/', list_books, name='list_books'),
   path('library/<int:pk>', LibraryDetailView.as_view(), name='library_list'),
-  path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('register/', user_register, name='register')
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.user_register, name='register'),
 ]
