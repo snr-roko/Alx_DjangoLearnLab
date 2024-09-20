fc32cfc6be785fd5a355404fef51cd220e9aef03
mr-roko <rbbagyei@gmail.com>
2024-09-20 22:56:02 +0000

    Blog model created_at field changed: Blog model created_at field has been replaced by published_date

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
