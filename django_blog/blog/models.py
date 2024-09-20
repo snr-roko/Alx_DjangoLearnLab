09fd4dd2e4e93dadd6835e9cbdf80eb596f6b213
mr-roko <rbbagyei@gmail.com>
2024-09-20 22:52:19 +0000

    Django Blog project created: blog app installed and database configured as well. Html templates and static css and javascript files have been added.

---
+++ django_blog/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
